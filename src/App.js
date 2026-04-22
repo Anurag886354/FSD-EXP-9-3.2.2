5192be6122484ae353bb3db76eb56bd63af23181
Anurag886354 <23bcy70265@cuchd.in>
2026-04-22 10:32:23 +0530

    Initial commit with CI/CD pipeline

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to CI/CD React App</h1>
+        <p>This is an automated deployment demonstration.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
